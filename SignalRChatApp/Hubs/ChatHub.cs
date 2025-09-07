a301e224406adf47206a401ef22395da6fbd55bc
shivam patil <shivampatil2810@gmail.com>
2025-09-07 14:52:18 +0530

    Add project files.

---
+++ SignalRChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System;
+using SignalRChatApp.Models;
+
+public class ChatHub : Hub
+{
+    private readonly ApplicationDbContext _context;
+
+    public ChatHub(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SendMessage(string user, string message)
+    {
+        var chatMessage = new ChatMessage
+        {
+            User = user,
+            Message = message,
+            Timestamp = DateTime.UtcNow
+        };
+
+        _context.ChatMessages.Add(chatMessage);
+        await _context.SaveChangesAsync();
+
+        await Clients.All.SendAsync("ReceiveMessage", user, message, chatMessage.Timestamp.ToString("HH:mm:ss"));
+    }
+}
---
+++ SignalRChatApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRChatApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+
+        }
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+    }
+}
